705aab69a530161fcc3955a2ab840778d55257d6
Wenxiao Zhang <41005111+XLechter@users.noreply.github.com>
2022-12-05 11:28:23 +0800

    Create resample_pcd.py

---
+++ resample_pcd.py
@@ -0,0 +1,6 @@
+def resample_pcd(pcd, n):
+    """Drop or duplicate points so that pcd has exactly n points"""
+    idx = np.random.permutation(pcd.shape[0])
+    if idx.shape[0] < n:
+        idx = np.concatenate([idx, np.random.randint(pcd.shape[0], size=n-pcd.shape[0])])
+    return pcd[idx[:n]]
