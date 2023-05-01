08ae260037e7b8121be286e5fff80b8eb097280f
Wenxiao Zhang <41005111+XLechter@users.noreply.github.com>
2023-05-01 23:40:23 +0800

    Update and rename file_list_operation to file_list_operation.py

--- file_list_operation.py
+++ file_list_operation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import blobfile as bf
-# get all the files in folder and subfolders in data_dir
 
+# get all the files in folder and subfolders in data_dir
 def _list_image_files_recursively(data_dir):
     results = []
     for entry in sorted(bf.listdir(data_dir)):
@@ -13,10 +13,17 @@ def _list_image_files_recursively(data_dir):
             results.extend(_list_image_files_recursively(full_path))
     return results
     
+    
 # select N items (files) from a list
-
 original_list = _list_image_files_recursively(data_dir)
 N = 5000
 selected_list = random.choices(original_list, k=N)
 print(len(selected_list))
+
+
+# get parent folder with os
+import os
+file_path = '/home/user/Downloads/test'
+parent_directory = os.path.dirname(file_path)
+print(parent_directory)
 
